8f0654d4e08938adf15838f7a5630281e9a3d156
hrv3str <r.novikov.zp.ua@gmail.com>
2023-10-25 21:40:02 +0300

    Create test cases for "reverseString" function

---
+++ scripts/reverseString.test.js
@@ -0,0 +1,29 @@
+import reverseString from './reverseString';
+
+it('Empty string input', () => {
+    expect(reverseString()).toBe('');
+});
+
+it('Single character input', () => {
+    expect(reverseString('h')).toBe('h');
+});
+
+it('String with multiple characters',() => {
+    expect(reverseString('hello')).toBe('olleh');
+});
+
+it('String with spaces', () => {
+    expect(reverseString('hello world')).toBe('dlrow olleh');
+});
+
+it('String with punctuation', () => {
+    expect(reverseString('hello world!!!')).toBe('!!!dlrow olleh');
+});
+
+it('String with uppercase and lovercase characters', () => {
+    expect(reverseString('hElLo WoRlD')).toBe('DlRoW oLlEh');
+});
+
+it('String with special characters', () => {
+    expect(reverseString('123@#abc')).toBe('cba#@321')
+});
